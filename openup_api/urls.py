ec4d46f0d44ca6dfe09ee2148d8523c9cbf2c3ab
swapnil15021997 <117801866+swapnil15021997@users.noreply.github.com>
2023-04-13 15:20:51 +0530

    all set repath used to servw static files
    all set repath used to servw static files

--- openup_api/urls.py
+++ openup_api/urls.py
@@ -93,6 +93,6 @@ urlpatterns = [
   
 # Employee active inactive
   path('employee-status', auth_views.employee_status,name='employee_status'),
- re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+  re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 ]
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
