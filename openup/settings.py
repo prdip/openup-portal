ba722254158f4236e0c200436366c7a27bf6ef37
SwapPathak <131649610+inlancersytem2@users.noreply.github.com>
2023-09-26 16:34:10 +0530

    minor fix
    minor fix

--- openup/settings.py
+++ openup/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY      =   'django-insecure-1g*)i!^u31(^hla+6a47dyhm#_vkz)xlv$c%%%_^vq%fe=y^lv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG           =   True
+DEBUG           =   False
 ADMIN_ENABLED   =   False
 # ALLOWED ALL HOST
 
@@ -127,7 +127,7 @@ EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_PORT          = env('EMAIL_PORT')
 
 
-print(EMAIL_HOST,EMAIL_HOST_USER)
+
 
 
 
