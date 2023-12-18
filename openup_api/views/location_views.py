64fc2be78b2084146a441a7ccd34b0300923c4d9
SwapPathak <swapnilpathak1997@gmail.com>
2023-12-18 11:23:14 +0530

    minor fix
    minor fix

--- openup_api/views/location_views.py
+++ openup_api/views/location_views.py
@@ -282,13 +282,13 @@ def employee_location(request):
         latitude     =    request.data.get('latitude',None)
         longitude    =    request.data.get('longitude',None)
 
-        if latitude is None and latitude == '':
+        if latitude is None:
             return JsonResponse({
                             "success"        :       0,
                             "message"        :      "Please provide lattitude"
             })
 
-        if longitude is None and latitude == '':
+        if longitude is None:
             return JsonResponse({
                             "success"        :       0,
                             "message"        :      "Please provide lattitude"
