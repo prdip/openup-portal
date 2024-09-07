065e0d0ededd1572bc1e46667da9f458486b209e
SwapPathak <117801866+swapnil15021997@users.noreply.github.com>
2024-09-07 10:44:29 +0530

    Update settings.py
    setting update

--- openup/settings.py
+++ openup/settings.py
@@ -46,8 +46,8 @@ ALLOWED_HOSTS   =   ['*']
 #  CELERY SETTINGS 
 
 os.environ.get('REDIS_URL')
-CELERY_BROKER_URL                   =       'redis://127.0.0.1:6379'
-CELERY_RESULT_BACKEND               =       'redis://127.0.0.1:6379'
+CELERY_BROKER_URL                   =       'redis://127.0.0.1:6380'
+CELERY_RESULT_BACKEND               =       'redis://127.0.0.1:6380'
 CELERY_ACCEPT_CONTENT               =       ['application/json']
 CELERY_RESULT_SERIALIZER            =       'json'
 CELERY_TASK_SERIALIZER              =       'json'
