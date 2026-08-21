5b3129111755985fd746bfefe05d180f2908d89b
Denish Borad <denishgithub4253@gmail.com>
2026-08-21 11:50:44 +0530

    Update settings.py

--- openup/settings.py
+++ openup/settings.py
@@ -18,7 +18,7 @@ import os
 # CODE FOR ENV
 import environ
 env = environ.Env()
-env.read_env('/var/www/openupweb/openup/.env')
+env.read_env(os.path.join(os.path.dirname(__file__), '.env'))
 # environ.Env.read_env(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 # CODE FOR ENV END
 
